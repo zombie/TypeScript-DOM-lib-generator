86c00dac549ce79f645362d4ed4eebe80eef4aaf
rohitverma007 <rohitverma@live.ca>
2016-01-25 00:43:40 -0500

    Adding webl and additional attributes to getContext method for HTMLCanvasElement

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -158,6 +158,7 @@ interface UIEventInit extends EventInit {
 }
 
 interface WebGLContextAttributes {
+    failIfMajorPerformanceCaveat?: boolean;
     alpha?: boolean;
     depth?: boolean;
     stencil?: boolean;
@@ -3761,9 +3762,9 @@ interface HTMLCanvasElement extends HTMLElement {
       * Returns an object that provides methods and properties for drawing and manipulating images and graphics on a canvas element in a document. A context object includes information about colors, line widths, fonts, and other graphic parameters that can be drawn on a canvas.
       * @param contextId The identifier (ID) of the type of canvas to create. Internet Explorer 9 and Internet Explorer 10 support only a 2-D context using canvas.getContext("2d"); IE11 Preview also supports 3-D or WebGL context using canvas.getContext("experimental-webgl");
       */
-    getContext(contextId: "2d"): CanvasRenderingContext2D;
-    getContext(contextId: "experimental-webgl"): WebGLRenderingContext;
-    getContext(contextId: string, ...args: any[]): CanvasRenderingContext2D | WebGLRenderingContext;
+    getContext(contextId: "2d", contextAttributes?: {alpha?: boolean; willReadFrequently?: boolean; storage?: boolean; [attrib: string]: boolean | string}): CanvasRenderingContext2D;
+    getContext(contextId: "webgl" | "experimental-webgl", contextAttributes: WebGLContextAttributes): WebGLRenderingContext;
+    getContext(contextId: string, contextAttributes?: {[prop: string]: boolean | string}): CanvasRenderingContext2D | WebGLRenderingContext;
     /**
       * Returns a blob object encoded as a Portable Network Graphics (PNG) format from a canvas image or drawing.
       */

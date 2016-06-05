f5e3fed12efb1d3bfeca5dd1277b2748f7363769
york yao <yaoao12306@outlook.com>
2016-06-05 17:00:59 +0800

    try to fix https://github.com/Microsoft/TypeScript/issues/8688

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -13134,7 +13134,6 @@ declare var XMLDocument: {
 }
 
 interface XMLHttpRequest extends EventTarget, XMLHttpRequestEventTarget {
-    msCaching: string;
     onreadystatechange: (ev: ProgressEvent) => any;
     readonly readyState: number;
     readonly response: any;
@@ -13146,6 +13145,7 @@ interface XMLHttpRequest extends EventTarget, XMLHttpRequestEventTarget {
     timeout: number;
     readonly upload: XMLHttpRequestUpload;
     withCredentials: boolean;
+    msCaching?: string;
     abort(): void;
     getAllResponseHeaders(): string;
     getResponseHeader(header: string): string | null;

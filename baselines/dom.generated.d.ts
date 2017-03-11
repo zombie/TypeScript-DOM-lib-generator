31d3c780a9c12f14754bdc85142d5c71e29297df
arjunyel <arjunyel@users.noreply.github.com>
2017-03-10 18:29:10 -0600

    Fixes DocumentFragment missing getElementById() (#212)
    * Fixes DocumentFragment DOM bug
    
    * Fixed signature
    
    * More consistent to document method

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -3450,6 +3450,7 @@ declare var Document: {
 }
 
 interface DocumentFragment extends Node, NodeSelector, ParentNode {
+    getElementById(elementId: string): HTMLElement | null;
 }
 
 declare var DocumentFragment: {

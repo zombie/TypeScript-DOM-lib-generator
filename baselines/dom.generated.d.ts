0f76e1f01b43b671ff9e6f0f5341e50d8d8c128e
ncoley <nrcoley@gmail.com>
2017-03-29 08:55:01 -0400

    Capitalize Blob in FormData type methods

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -3796,12 +3796,12 @@ declare var FocusNavigationEvent: {
 }
 
 interface FormData {
-    append(name: string, value: string | blob, fileName?: string): void;
+    append(name: string, value: string | Blob, fileName?: string): void;
     delete(name: string): void;
     get(name: string): FormDataEntryValue | null;
     getAll(name: string): FormDataEntryValue[];
     has(name: string): boolean;
-    set(name: string, value: string | blob, fileName?: string): void;
+    set(name: string, value: string | Blob, fileName?: string): void;
 }
 
 declare var FormData: {

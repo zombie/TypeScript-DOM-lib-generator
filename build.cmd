cba9e4811ca49a6e607826e6666efb0c083bbc52
Mohamed Hegazy <mhegazy@microsoft.com>
2018-02-26 16:38:10 -0800

    Add some build scripts

--- build.cmd
+++ build.cmd
@@ -1,26 +1,5 @@
 @echo off
 cls
 
-if not exist .paket mkdir .paket
-
-if not exist .paket/paket.bootstrapper.exe (
-  @echo "Installing Paket"
-  Powershell.exe -NoProfile -Command "Invoke-WebRequest -Uri https://github.com/fsprojects/Paket/releases/download/2.12.5/paket.bootstrapper.exe -OutFile .paket\paket.bootstrapper.exe"
-
-
-  .paket\paket.bootstrapper.exe prerelease
-  if errorlevel 1 (
-    exit /b %errorlevel%
-  )
-)
-
-if not exist paket.lock (
-  @echo "Installing dependencies"
-  .paket\paket.exe install
-) else (
-  @echo "Restoring dependencies"
-  .paket\paket.exe restore
-)
-
 @echo "Building..."
-packages\FAKE\tools\FAKE.exe %* --fsiargs --optimize+ build.fsx
+npm run build  & npm run test

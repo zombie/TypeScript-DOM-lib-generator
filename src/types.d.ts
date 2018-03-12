aa9d2606765804182926e27ba0c36f7f9456bea9
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-03-12 13:27:13 +0900

    use interfaces

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,12 +1,12 @@
-export type Typed = {
+export interface Typed {
     "type": string | Typed[];
     "subtype"?: Typed;
     "nullable"?: 1;
     "type-original"?: string;
     "override-type"?: string;
-};
+}
 
-export type Param = {
+export interface Param {
     "name": string;
     "type": string | Typed[];
     "subtype"?: Typed;
@@ -15,18 +15,18 @@ export type Param = {
     "optional"?: 1;
     "variadic"?: 1;
     "treat-null-as"?: string;
-};
+}
 
-export type Signature = {
+export interface Signature {
     "type": string | Typed[];
     "subtype"?: Typed;
     "nullable"?: 1;
     "type-original": string;
     "param"?: Param[];
     "param-min-required"?: number,
-};
+}
 
-export type Member = {
+export interface Member {
     "name": string;
     "type": string | Typed[];
     "subtype"?: Typed;
@@ -36,9 +36,9 @@ export type Member = {
     "required"?: 1;
     "override-type"?: string;
     "specs"?: string;
-};
+}
 
-export type Property = {
+export interface Property {
     "name": string;
     "event-handler"?: string;
     "type": string | Typed[];
@@ -77,9 +77,9 @@ export type Property = {
     "interop"?: 1;
     "exposed"?: string;
     "constant"?: 1;
-};
+}
 
-export type Event = {
+export interface Event {
     "name": string;
     "type": string;
     "dispatch"?: string;
@@ -91,9 +91,9 @@ export type Event = {
     "tags"?: string;
     "aliases"?: string;
     "specs"?: string;
-};
+}
 
-export type Method = {
+export interface Method {
     "name": string;
     "tags"?: string;
     "static"?: 1;
@@ -108,30 +108,30 @@ export type Method = {
     "exposed"?: string;
     "deprecated"?: 1;
     "signature": Signature[];
-};
+}
 
-export type CallbackFunction = {
+export interface CallbackFunction {
     "name": string;
     "callback": 1;
     "signature": Signature[];
     "tags"?: string;
     "override-signatures"?: string[];
     "specs"?: string;
-};
+}
 
-export type Constructor = {
+export interface Constructor {
     "signature": Signature[];
     "comment"?: string;
     "specs"?: string;
-};
+}
 
-export type NamedConstructor = {
+export interface NamedConstructor {
     "name": string;
     "signature": Signature[];
     "specs"?: string;
-};
+}
 
-export type Constant = {
+export interface Constant {
     "name": string;
     "type": string | Typed[];
     "subtype"?: Typed;
@@ -141,46 +141,46 @@ export type Constant = {
     "tags"?: string
     "exposed"?: string;
     "specs"?: string;
-};
+}
 
-export type ParsedAttribute ={
+export interface ParsedAttribute{
     "enum-values"?: string;
     "name": string;
     "value-syntax"?: string;
-};
+}
 
-export type Element = {
+export interface Element {
     "name": string;
     "namespace"?: string;
     "html-self-closing"?: string;
     "specs"?: string;
-};
+}
 
-export type Interface = {
+export interface Interface {
     "name": string;
     "extends": string;
     "constants"?: {
         "constant": Record<string, Constant>;
-    };
+    }
     "methods": {
         "method": Record<string, Method>;
-    };
+    }
     "events"?: {
         "event": Event[];
-    };
+    }
     "properties"?: {
         "property": Record<string, Property>;
-    };
+    }
     "constructor"?: Constructor;
     "secure-context"?: string;
     "implements"?: string[];
     "static"?: 1;
     "anonymous-methods"?: {
         "method": Method[];
-    };
+    }
     "anonymous-content-attributes"?: {
         "parsedattribute": ParsedAttribute[];
-    };
+    }
     "element"?: Element[];
     "named-constructor"?: NamedConstructor;
     "override-builtins"?: string;
@@ -194,50 +194,50 @@ export type Interface = {
     "overide-index-signatures"?: string[];
     "specs"?: string;
     "iterable"?: "value" | "pair" | "pair-iterator";
-};
+}
 
-export type Enum = {
+export interface Enum {
     "name": string;
     "value": string[];
     "specs"?: string;
-};
+}
 
-export type TypeDef = {
+export interface TypeDef {
     "new-type": string;
     "type": string;
     "override-type"?: string;
-};
+}
 
-export type Dictionary = {
+export interface Dictionary {
     "name": string;
     "extends": string;
     "members": {
         "member": Record<string, Member>;
-    };
+    }
     "specs"?: string;
     "type-parameters"?: string[];
-};
+}
 
-export type WebIdl = {
+export interface WebIdl {
     "callback-functions"?: {
         "callback-function": Record<string, CallbackFunction>;
     },
     "callback-interfaces"?: {
         "interface": Record<string, Interface>;
-    };
+    }
     "dictionaries"?: {
         "dictionary": Record<string, Dictionary>;
-    };
+    }
     "enums"?: {
         "enum": Record<string, Enum>;
-    };
+    }
     "interfaces"?: {
         "interface": Record<string, Interface>;
-    };
+    }
     "mixins"?: {
         "mixin": Record<string, Interface>;
-    };
+    }
     "typedefs"?: {
         "typedef": TypeDef[];
-    };
-};
+    }
+}

accf142fd824088e612859e193b3bf46c8b69b1c
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-04-13 07:05:57 +0900

    return to previous union behavior

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -5082,14 +5082,12 @@ declare var FocusNavigationEvent: {
 };
 
 interface FormData {
-    append(name: string, value: string): void;
-    append(name: string, blobValue: Blob, filename?: string): void;
+    append(name: string, value: string | Blob, fileName?: string): void;
     delete(name: string): void;
     get(name: string): FormDataEntryValue | null;
     getAll(name: string): FormDataEntryValue[];
     has(name: string): boolean;
-    set(name: string, value: string): void;
-    set(name: string, blobValue: Blob, filename?: string): void;
+    set(name: string, value: string | Blob, fileName?: string): void;
     forEach(callbackfn: (value: FormDataEntryValue, key: string, parent: FormData) => void, thisArg?: any): void;
 }
 
--- baselines/webworker.generated.d.ts
+++ baselines/webworker.generated.d.ts
@@ -641,14 +641,12 @@ declare var FileReaderSync: {
 };
 
 interface FormData {
-    append(name: string, value: string): void;
-    append(name: string, blobValue: Blob, filename?: string): void;
+    append(name: string, value: string | Blob, fileName?: string): void;
     delete(name: string): void;
     get(name: string): FormDataEntryValue | null;
     getAll(name: string): FormDataEntryValue[];
     has(name: string): boolean;
-    set(name: string, value: string): void;
-    set(name: string, blobValue: Blob, filename?: string): void;
+    set(name: string, value: string | Blob, fileName?: string): void;
     forEach(callbackfn: (value: FormDataEntryValue, key: string, parent: FormData) => void, thisArg?: any): void;
 }
 

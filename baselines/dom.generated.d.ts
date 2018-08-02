bd6320558e7782d81fb4cf6ddd4e6cd8737bfb93
Mohamed Hegazy <mhegazy@microsoft.com>
2018-08-02 09:48:00 -0700

    Fix https://github.com/Microsoft/TypeScript/issues/26133: Add `HTMLObjectElement.reportValidity`

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -7398,6 +7398,7 @@ interface HTMLObjectElement extends HTMLElement, GetSVGDocument {
      * Returns whether a form will validate when it is submitted, without having to submit it.
      */
     checkValidity(): boolean;
+    reportValidity(): boolean;
     /**
      * Sets a custom error message that is displayed when a form is submitted.
      * @param error Sets a custom error message that is displayed when a form is submitted.

af5388111df524a5b0fb78de92e8d7e7b2ff675a
Mattias Buelens <mattias@buelens.com>
2018-11-03 10:42:47 +0100

    Simplify pipeThrough

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -11877,7 +11877,7 @@ interface ReadableStream<R = any> {
     cancel(reason?: any): Promise<void>;
     getReader(options: { mode: "byob" }): ReadableStreamBYOBReader;
     getReader(): ReadableStreamDefaultReader<R>;
-    pipeThrough<T extends ReadableStream>({ writable, readable }: { writable: WritableStream<R>, readable: T }, options?: PipeOptions): T;
+    pipeThrough<T>({ writable, readable }: { writable: WritableStream<R>, readable: ReadableStream<T> }, options?: PipeOptions): ReadableStream<T>;
     pipeTo(dest: WritableStream<R>, options?: PipeOptions): Promise<void>;
     tee(): [ReadableStream<R>, ReadableStream<R>];
 }

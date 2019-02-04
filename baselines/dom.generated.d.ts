e82f1605ded78c84f561b79081be0018d7c8d8b2
pp <pat12121985@gmail.com>
2019-02-04 19:30:34 +0100

    Added willChange Property to the CSSStyleDeclaration interface

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -2917,6 +2917,7 @@ interface CSSStyleDeclaration {
     whiteSpace: string | null;
     widows: string | null;
     width: string | null;
+    willChange: string | null;
     wordBreak: string | null;
     wordSpacing: string | null;
     wordWrap: string | null;

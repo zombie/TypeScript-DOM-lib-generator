3cd60c99bba2303c3e6189e5c394fcaf3d174b10
Kagami Sascha Rosylight <saschanaz@outlook.com>
2020-10-25 03:25:14 +0100

    chore: remove unimplemented WebVR APIs

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -16124,7 +16124,6 @@ interface VRPose {
     readonly linearVelocity: Float32Array | null;
     readonly orientation: Float32Array | null;
     readonly position: Float32Array | null;
-    readonly timestamp: number;
 }
 
 declare var VRPose: {
@@ -18440,9 +18439,6 @@ interface WindowEventMap extends GlobalEventHandlersEventMap, WindowEventHandler
     "vrdisplayconnect": Event;
     "vrdisplaydeactivate": Event;
     "vrdisplaydisconnect": Event;
-    "vrdisplayfocus": Event;
-    "vrdisplaypointerrestricted": Event;
-    "vrdisplaypointerunrestricted": Event;
     "vrdisplaypresentchange": Event;
     "waiting": Event;
 }
@@ -18505,9 +18501,6 @@ interface Window extends EventTarget, AnimationFrameProvider, GlobalEventHandler
     onvrdisplayconnect: ((this: Window, ev: Event) => any) | null;
     onvrdisplaydeactivate: ((this: Window, ev: Event) => any) | null;
     onvrdisplaydisconnect: ((this: Window, ev: Event) => any) | null;
-    onvrdisplayfocus: ((this: Window, ev: Event) => any) | null;
-    onvrdisplaypointerrestricted: ((this: Window, ev: Event) => any) | null;
-    onvrdisplaypointerunrestricted: ((this: Window, ev: Event) => any) | null;
     onvrdisplaypresentchange: ((this: Window, ev: Event) => any) | null;
     opener: any;
     /** @deprecated */
@@ -19551,9 +19544,6 @@ declare var onvrdisplayblur: ((this: Window, ev: Event) => any) | null;
 declare var onvrdisplayconnect: ((this: Window, ev: Event) => any) | null;
 declare var onvrdisplaydeactivate: ((this: Window, ev: Event) => any) | null;
 declare var onvrdisplaydisconnect: ((this: Window, ev: Event) => any) | null;
-declare var onvrdisplayfocus: ((this: Window, ev: Event) => any) | null;
-declare var onvrdisplaypointerrestricted: ((this: Window, ev: Event) => any) | null;
-declare var onvrdisplaypointerunrestricted: ((this: Window, ev: Event) => any) | null;
 declare var onvrdisplaypresentchange: ((this: Window, ev: Event) => any) | null;
 declare var opener: any;
 /** @deprecated */

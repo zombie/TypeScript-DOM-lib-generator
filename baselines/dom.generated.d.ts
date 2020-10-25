647440a6c8f784f15ccc27617a3a5d3450e316c8
Kagami Sascha Rosylight <saschanaz@outlook.com>
2020-10-25 02:09:41 +0200

    chore: remove MediaStreamTrack isolation

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -10398,7 +10398,6 @@ declare var MediaStreamEvent: {
 
 interface MediaStreamTrackEventMap {
     "ended": Event;
-    "isolationchange": Event;
     "mute": Event;
     "unmute": Event;
 }
@@ -10407,12 +10406,10 @@ interface MediaStreamTrackEventMap {
 interface MediaStreamTrack extends EventTarget {
     enabled: boolean;
     readonly id: string;
-    readonly isolated: boolean;
     readonly kind: string;
     readonly label: string;
     readonly muted: boolean;
     onended: ((this: MediaStreamTrack, ev: Event) => any) | null;
-    onisolationchange: ((this: MediaStreamTrack, ev: Event) => any) | null;
     onmute: ((this: MediaStreamTrack, ev: Event) => any) | null;
     onunmute: ((this: MediaStreamTrack, ev: Event) => any) | null;
     readonly readyState: MediaStreamTrackState;

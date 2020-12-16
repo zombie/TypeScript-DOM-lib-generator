e78d6213936c0dad92caf73eef277041b155e616
Orta <git@orta.io>
2020-12-16 15:47:38 +0000

    Get the diff from the subfolder

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,7 @@
 const {message} = require("danger")
 const {readFileSync, existsSync} = require("fs")
 
-const diffPath = "baseline-changes.diff"
+const diffPath = "./TypeScript/baseline-changes.diff"
 if (existsSync(diffPath)) {
   const diffContents = readFileSync(diffPath, "utf8")
   message(diffContents)

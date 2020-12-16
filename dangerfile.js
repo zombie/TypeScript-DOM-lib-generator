84791e331298e650b009d8037ce934d65dc3614d
Orta <git@orta.io>
2020-12-16 16:14:46 +0000

    Read the diff and only print the interesting stuff

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,29 @@
 const {message} = require("danger")
 const {readFileSync, existsSync} = require("fs")
+const parseDiff = require("parse-diff")
 
 const diffPath = "./TypeScript/baseline-changes.diff"
 if (existsSync(diffPath)) {
   const diffContents = readFileSync(diffPath, "utf8")
-  message(diffContents)
+  const diffedFiles = parseDiff(diffContents)
+  
+  const uninterestingFiles = [".generated.d.ts", "globalThisBlockscopedProperties.types", "mappedTypeRecursiveInference.types"]
+  const withoutKnownNormalFails = diffedFiles.filter(diff => {
+    return uninterestingFiles.filter(suffix => diff.to?.endsWith(suffix)).length > 0
+  })
+
+  const md = ["## Changed baselines from the TypeScript test suite"]
+
+  withoutKnownNormalFails.forEach(diff => {
+    md.push(`#### ${diff.to || diff.from}}`)
+
+    md.push("```diff")
+    diff.chunks.forEach(chunk => {
+      md.push(chunk.content)
+    })
+    md.push("```")
+  })
+
+
+  message(md.join("\n"))
 }

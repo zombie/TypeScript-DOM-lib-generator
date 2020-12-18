66eba48ce8eccc9f42c4594c9346290bacbb6411
Kagami Sascha Rosylight <saschanaz@outlook.com>
2020-12-18 21:57:02 +0100

    feat(types): add mediasession

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -659,6 +659,12 @@ interface MediaEncryptedEventInit extends EventInit {
     initDataType?: string;
 }
 
+interface MediaImage {
+    sizes?: string;
+    src: string;
+    type?: string;
+}
+
 interface MediaKeyMessageEventInit extends EventInit {
     message: ArrayBuffer;
     messageType: MediaKeyMessageType;
@@ -680,9 +686,29 @@ interface MediaKeySystemMediaCapability {
     robustness?: string;
 }
 
+interface MediaMetadataInit {
+    album?: string;
+    artist?: string;
+    artwork?: MediaImage[];
+    title?: string;
+}
+
+interface MediaPositionState {
+    duration?: number;
+    playbackRate?: number;
+    position?: number;
+}
+
 interface MediaQueryListEventInit extends EventInit {
     matches?: boolean;
     media?: string;
+}
+
+interface MediaSessionActionDetails {
+    action: MediaSessionAction;
+    fastSeek?: boolean | null;
+    seekOffset?: number | null;
+    seekTime?: number | null;
 }
 
 interface MediaStreamAudioSourceOptions {
@@ -9523,6 +9549,18 @@ declare var MediaList: {
     new(): MediaList;
 };
 
+interface MediaMetadata {
+    album: string;
+    artist: string;
+    artwork: ReadonlyArray<MediaImage>;
+    title: string;
+}
+
+declare var MediaMetadata: {
+    prototype: MediaMetadata;
+    new(init?: MediaMetadataInit): MediaMetadata;
+};
+
 interface MediaQueryListEventMap {
     "change": MediaQueryListEvent;
 }
@@ -9555,6 +9593,18 @@ interface MediaQueryListEvent extends Event {
 declare var MediaQueryListEvent: {
     prototype: MediaQueryListEvent;
     new(type: string, eventInitDict?: MediaQueryListEventInit): MediaQueryListEvent;
+};
+
+interface MediaSession {
+    metadata: MediaMetadata | null;
+    playbackState: MediaSessionPlaybackState;
+    setActionHandler(action: MediaSessionAction, handler: MediaSessionActionHandler | null): void;
+    setPositionState(state?: MediaPositionState): void;
+}
+
+declare var MediaSession: {
+    prototype: MediaSession;
+    new(): MediaSession;
 };
 
 interface MediaSourceEventMap {
@@ -9962,6 +10012,7 @@ interface Navigator extends NavigatorAutomationInformation, NavigatorConcurrentH
     readonly geolocation: Geolocation;
     readonly maxTouchPoints: number;
     readonly mediaDevices: MediaDevices;
+    readonly mediaSession: MediaSession;
     readonly permissions: Permissions;
     readonly pointerEnabled: boolean;
     readonly serviceWorker: ServiceWorkerContainer;
@@ -17278,6 +17329,10 @@ interface MSLaunchUriCallback {
     (): void;
 }
 
+interface MediaSessionActionHandler {
+    (details: MediaSessionActionDetails): void;
+}
+
 interface MutationCallback {
     (mutations: MutationRecord[], observer: MutationObserver): void;
 }
@@ -18114,6 +18169,8 @@ type MediaKeyMessageType = "individualization-request" | "license-release" | "li
 type MediaKeySessionType = "persistent-license" | "persistent-usage-record" | "temporary";
 type MediaKeyStatus = "expired" | "internal-error" | "output-downscaled" | "output-restricted" | "released" | "status-pending" | "usable" | "usable-in-future";
 type MediaKeysRequirement = "not-allowed" | "optional" | "required";
+type MediaSessionAction = "nexttrack" | "pause" | "play" | "previoustrack" | "seekbackward" | "seekforward" | "seekto" | "skipad" | "stop";
+type MediaSessionPlaybackState = "none" | "paused" | "playing";
 type MediaStreamTrackState = "ended" | "live";
 type NavigationType = "back_forward" | "navigate" | "prerender" | "reload";
 type NotificationDirection = "auto" | "ltr" | "rtl";

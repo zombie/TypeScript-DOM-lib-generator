357169aac4841e904aa3e2507ff868cbadc6f121
Kagami Sascha Rosylight <saschanaz@outlook.com>
2020-12-20 16:00:11 +0100

    feat(types): add mediacapture-fromelement

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -6123,6 +6123,7 @@ interface HTMLCanvasElement extends HTMLElement {
      * Gets or sets the width of a canvas element on a document.
      */
     width: number;
+    captureStream(frameRequestRate?: number): MediaStream;
     /**
      * Returns an object that provides methods and properties for drawing and manipulating images and graphics on a canvas element in a document. A context object includes information about colors, line widths, fonts, and other graphic parameters that can be drawn on a canvas.
      * @param contextId The identifier (ID) of the type of canvas to create. Internet Explorer 9 and Internet Explorer 10 support only a 2-D context using canvas.getContext("2d"); IE11 Preview also supports 3-D or WebGL context using canvas.getContext("experimental-webgl");

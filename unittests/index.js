0afeec5574f9c251ca67facaa4cea2f48738ef3c
Kagami Sascha Rosylight <saschanaz@outlook.com>
2020-12-25 20:54:08 +0100

    chore(unittests): use --types to disable types packages

--- unittests/index.js
+++ unittests/index.js
@@ -3,7 +3,7 @@ import { readdir } from "fs/promises";
 
 for (const filename of await readdir(new URL("files/", import.meta.url))) {
   if (filename.endsWith(".ts")) {
-    execSync(`npx tsc generated/dom.generated.d.ts unittests/files/${filename} --target es2020 --lib es2020 --noEmit --skipLibCheck`, {
+    execSync(`npx tsc generated/dom.generated.d.ts unittests/files/${filename} --target es2020 --lib es2020 --types --noEmit`, {
       stdio: "inherit"
     });
   }

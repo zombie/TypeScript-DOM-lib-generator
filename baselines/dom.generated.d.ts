0f4a1bd311af0ccf4037fe9963c015f27d6258e4
Kagami Sascha Rosylight <saschanaz@outlook.com>
2021-07-17 02:23:56 +0200

    Add HTMLElement-returning getElementById to DocumentFragment (#1071)
    This is copying what `document.getElementById` and `document.body.ownerDocument` currently looks like.
    
    Regressed by b598925.

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -4724,6 +4724,8 @@ interface DocumentAndElementEventHandlers {
 
 /** A minimal document object that has no parent. It is used as a lightweight version of Document that stores a segment of a document structure comprised of nodes just like a standard document. The key difference is that because the document fragment isn't part of the active document tree structure, changes made to the fragment don't affect the document, cause reflow, or incur any performance impact that can occur when changes are made. */
 interface DocumentFragment extends Node, NonElementParentNode, ParentNode {
+    readonly ownerDocument: Document;
+    getElementById(elementId: string): HTMLElement | null;
 }
 
 declare var DocumentFragment: {

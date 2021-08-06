76512b377cc4ec340cf023c5cd82f81664546970
Orta Therox <ortam@microsoft.com>
2021-08-06 15:05:54 +0100

    Removes the nullable attribute from window.parent (#1089)
    * Remvoes the nullable attribute from parent
    
    * Adds a comment about why it's not null
    
    * Expand copy
    
    * Update inputfiles/comments.json
    
    Co-authored-by: Kagami Sascha Rosylight <saschanaz@outlook.com>
    
    * Fix baselines
    
    Co-authored-by: Kagami Sascha Rosylight <saschanaz@outlook.com>

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -17230,7 +17230,12 @@ interface Window extends EventTarget, AnimationFrameProvider, GlobalEventHandler
     readonly pageXOffset: number;
     /** @deprecated This is a legacy alias of `scrollY`. */
     readonly pageYOffset: number;
-    readonly parent: WindowProxy | null;
+    /**
+     * Refers to either the parent WindowProxy, or itself.
+     *
+     * It can rarely be null e.g. for contentWindow of an iframe that is already removed from the parent.
+     */
+    readonly parent: WindowProxy;
     /**
      * Returns true if the personal bar is visible; otherwise, returns false.
      */
@@ -18294,7 +18299,12 @@ declare var outerWidth: number;
 declare var pageXOffset: number;
 /** @deprecated This is a legacy alias of `scrollY`. */
 declare var pageYOffset: number;
-declare var parent: WindowProxy | null;
+/**
+ * Refers to either the parent WindowProxy, or itself.
+ *
+ * It can rarely be null e.g. for contentWindow of an iframe that is already removed from the parent.
+ */
+declare var parent: WindowProxy;
 /**
  * Returns true if the personal bar is visible; otherwise, returns false.
  */

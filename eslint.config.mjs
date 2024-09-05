e342c0eff22568c92dc459a4fd7ff8cbd85fc563
Mattias Buelens <649348+MattiasBuelens@users.noreply.github.com>
2024-09-05 22:35:35 +0200

    Migrate to ESLint flat config (#1784)
    * Migrate to ESLint flat config
    
    * Updates from editor
    
    Fix package-lock.json by installing 8.3.0 and copying result over.
    
    ---------
    
    Co-authored-by: Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,20 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
+// @ts-check
+
+import typescriptEslint from "typescript-eslint";
 import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default [
-  ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ),
+export default typescriptEslint.config(
+  js.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  prettierRecommended,
   {
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-    },
-
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.browser,
       },
-
-      parser: tsParser,
     },
 
     rules: {
@@ -41,4 +24,4 @@ export default [
       "@typescript-eslint/no-var-requires": 0,
     },
   },
-];
+);
